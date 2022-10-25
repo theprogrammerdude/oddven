ba67551c49b39eed0c43938d46099ab95d6f56ac
TheProgrammerDude <shas1980805@gmail.com>
2022-10-25 17:21:27 +0530

    odd even v1 repo for npm package

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+checkOddEven = (n) => {
+  if (n % 2 === 0) {
+    // console.log(`${n} is an even number`);
+    return "odd";
+  } else {
+    // console.log(`${n} is an odd number`);
+    return "even";
+  }
+};
